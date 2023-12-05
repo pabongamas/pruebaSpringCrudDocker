d9fad8d86c9fdf0157c77849df7fb72481efaa21
pabongamas <72658365+pabongamas@users.noreply.github.com>
2023-12-05 01:18:50 -0500

    first commit

---
+++ src/main/java/com/example/prueba/PruebaApplication.java
@@ -0,0 +1,14 @@
+package com.example.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		SpringApplication.run(PruebaApplication.class, args);
+	}
+
+}
